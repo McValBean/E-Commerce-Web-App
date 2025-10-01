068fc18d994eb0cb3471bbdb72aad84761fcb221
Valentine McBean-Willis <mcvalbean@gmail.com>
2025-10-01 14:00:53 +0000

    changes

--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -12,6 +12,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { useState } from "react"
 
+
 export function LoginForm({
   className,
   ...props
